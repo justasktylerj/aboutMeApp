b6989311ad005755b8c8d103b93f36dd56d4ed6f
Tyler jarrard <tjar2074@740s-pl-104.local>
2016-01-08 09:54:39 -0700

    stuff

---
+++ aboutMeApp/GamesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GamesViewController.swift
+//  aboutMeApp
+//
+//  Created by Jarrard, Tyler on 1/8/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import Foundation
+class GamesViewController : UIViewController
+{
+
+}
+
---
+++ aboutMeApp/VideoGames.swift
@@ -0,0 +1,13 @@
+//
+//  VideoGames.swift
+//  aboutMeApp
+//
+//  Created by Jarrard, Tyler on 1/8/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import Foundation
+class VideoGamesViewController : UIViewController
+{
+
+}
